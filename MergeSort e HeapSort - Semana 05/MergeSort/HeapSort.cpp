961a14fee9e262866374aa8d3d39b6394121fee2
YondaimeDinho <DouglasLuis2017@outlook.com.br>
2023-03-17 14:31:04 -0300

    modified:   MergeSort e HeapSort - Semana 05/MergeSort/HeapSort.cpp 	new file:   MergeSort e HeapSort - Semana 05/sorting.h

--- MergeSort e HeapSort - Semana 05/MergeSort/HeapSort.cpp
+++ MergeSort e HeapSort - Semana 05/MergeSort/HeapSort.cpp
@@ -3,7 +3,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <sorting.h>
+#include "sorting.h"
 
 int main() {
   int n, i, main_calls = 0, swaps = 0, comparisons = 0;
---
+++ MergeSort e HeapSort - Semana 05/sorting.h
@@ -0,0 +1,8 @@
+//Douglas Leonel de Almeida
+//Matricula - 2110213
+
+#ifndef sorting.h
+#define sorting.h
+
+void MergeSort(int* vetor, int esquerda, int direita, int* main_calls, int* swaps, int* comparisons);
+void HeapSort(int* vetor, int n, int* main_calls, int* swaps, int* comparisons);
