189c8134e0f7b9bc8a170d1302c76194f4855f5d
YondaimeDinho <DouglasLuis2017@outlook.com.br>
2023-03-23 22:20:43 -0300

    modified:   APR - Ciclo 1/script.cc

--- APR - Ciclo 1/script.cc
+++ APR - Ciclo 1/script.cc
@@ -1,116 +1,90 @@
-//Douglas Leonel de Almeida
-//Matricula - 2110213
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-#include <iostream>
-#include <vector>
+typedef struct Paciente {
+    int idade;
+    char sexo;
+    float renda;
+    char bairro[50];
+    int qtd_moradores;
+} Paciente;
 
-using namespace std;
-
-//Função para imprimir um vetor
-void printVector(vector<int> v) {
-    for (int i = 0; i < v.size(); i++) {
-        cout << v[i] << " ";
-    }
-    cout << endl;
-}
-
-//Função para ordenar um vetor utilizando o algoritmo QuickSort
-void quickSort(vector<int> &v, int inicio, int fim) {
-    if (inicio < fim) {
-        int pivo = v[inicio];
-        int i = inicio + 1, j = fim;
-
-        while (i <= j) {
-            if (v[i] <= pivo) {
-                i++;
-            } else if (v[j] > pivo) {
-                j--;
-            } else {
-                swap(v[i], v[j]);
-            }
-        }
-
-        swap(v[inicio], v[j]);
-
-        quickSort(v, inicio, j - 1);
-        quickSort(v, j + 1, fim);
-    }
-}
-
-//Função para ordenar um vetor utilizando o algoritmo Bubble Sort
-void bubbleSort(vector<int> &v) {
-    int n = v.size();
-
-    for (int i = 0; i < n - 1; i++) {
-        for (int j = 0; j < n - i - 1; j++) {
-            if (v[j] > v[j + 1]) {
-                swap(v[j], v[j + 1]);
+void bubbleSort(Paciente* pacientes, int n) {
+    int i, j;
+    Paciente temp;
+    for(i = 0; i < n; i++) {
+        for(j = 0; j < n-i-1; j++) {
+            if(pacientes[j].idade > pacientes[j+1].idade) {
+                temp = pacientes[j];
+                pacientes[j] = pacientes[j+1];
+                pacientes[j+1] = temp;
             }
         }
     }
 }
 
-//Função para ordenar um vetor utilizando o algoritmo Selection Sort
-void selectionSort(vector<int> &v) {
-    int n = v.size();
-
-    for (int i = 0; i < n - 1; i++) {
-        int minIndex = i;
-
-        for (int j = i + 1; j < n; j++) {
-            if (v[j] < v[minIndex]) {
-                minIndex = j;
+void selectionSort(Paciente* pacientes, int n) {
+    int i, j, min_idx;
+    Paciente temp;
+    for(i = 0; i < n-1; i++) {
+        min_idx = i;
+        for(j = i+1; j < n; j++) {
+            if(pacientes[j].idade < pacientes[min_idx].idade) {
+                min_idx = j;
             }
         }
-
-        swap(v[i], v[minIndex]);
+        temp = pacientes[min_idx];
+        pacientes[min_idx] = pacientes[i];
+        pacientes[i] = temp;
     }
 }
 
-//Função para ordenar um vetor utilizando o algoritmo Insertion Sort
-void insertionSort(vector<int> &v) {
-    int n = v.size();
-
-    for (int i = 1; i < n; i++) {
-        int key = v[i];
-        int j = i - 1;
-
-        while (j >= 0 && v[j] > key) {
-            v[j + 1] = v[j];
-            j--;
+void insertionSort(Paciente* pacientes, int n) {
+    int i, j;
+    Paciente key;
+    for(i = 1; i < n; i++) {
+        key = pacientes[i];
+        j = i - 1;
+        while(j >= 0 && pacientes[j].idade > key.idade) {
+            pacientes[j+1] = pacientes[j];
+            j = j - 1;
         }
-
-        v[j + 1] = key;
+        pacientes[j+1] = key;
     }
 }
 
-//Função principal para teste
+void printPacientes(Paciente* pacientes, int n) {
+    int i;
+    for(i = 0; i < n; i++) {
+        printf("Idade: %d, Sexo: %c, Renda: %.2f, Bairro: %s, Qtd. Moradores: %d\n",
+               pacientes[i].idade, pacientes[i].sexo, pacientes[i].renda, pacientes[i].bairro, pacientes[i].qtd_moradores);
+    }
+}
+
 int main() {
-    std::vector<int> v;
-	v.push_back(5);
-	v.push_back(3);
-	v.push_back(8);
-	v.push_back(1);
-	v.push_back(2);
-	v.push_back(7);
-	v.push_back(4);
-	v.push_back(6);
+    Paciente pacientes[5] = {{20, 'F', 2500.00, "Centro", 2},
+                             {23, 'M', 4500.00, "Setor Sul", 3},
+                             {60, 'M', 9000.00, "Jardim América", 4},
+                             {57, 'F', 1200.00, "Jardim das Américas 2 etapa", 1},
+                             {35, 'M', 2000.00, "Recanto do Sol", 2}};
 
-    //Ordenação utilizando o algoritmo QuickSort
-    quickSort(v, 0, v.size() - 1);
-    printVector(v);
-
-    //Ordenação utilizando o algoritmo Bubble Sort
-    bubbleSort(v);
-    printVector(v);
-
-    //Ordenação utilizando o algoritmo Selection Sort
-    selectionSort(v);
-    printVector(v);
-
-    //Ordenação utilizando o algoritmo Insertion Sort
-    insertionSort(v);
-    printVector(v);
+    int n = sizeof(pacientes)/sizeof(pacientes[0]);
+    
+    // Bubble Sort
+    bubbleSort(pacientes, n);
+    printf("Bubble Sort:\n");
+    printPacientes(pacientes, n);
+    
+    // Selection Sort
+    selectionSort(pacientes, n);
+    printf("\nSelection Sort:\n");
+    printPacientes(pacientes, n);
+    
+    // Insertion Sort
+    insertionSort(pacientes, n);
+    printf("\nInsertion Sort:\n");
+    printPacientes(pacientes, n);
 
     return 0;
 }
